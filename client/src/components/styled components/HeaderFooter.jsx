4c4fdf0fb5e696e14e230dd590782779b5801b56
JasenABaker <JasenABaker@gmail.com>
2018-02-25 00:19:56 -0500

    created header component

---
+++ client/src/components/styled components/HeaderFooter.jsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+
+const Header = styled.header`
+    height: 10vh;
+    width: 100vw;
+    background-color:#D00000;
+    border-bottom: 5px solid black;
+
+`
+
+
+
+
+export default Header
