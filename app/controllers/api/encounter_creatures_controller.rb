9a70b8c2b33e1028254347de7d550cdbdcd48067
JasenABaker <JasenABaker@gmail.com>
2018-02-25 00:08:20 -0500

    added encounter creature controller

---
+++ app/controllers/api/encounter_creatures_controller.rb
@@ -0,0 +1,32 @@
+class Api::EncounterCreaturesController < ApplicationController
+    def index
+        @encounter_creatures = Encounter_Creatures.all
+        
+        render json: @encounter_creatures
+        end
+    
+        def create
+        @encounter_creature = Encounter_Creatures.create!(encounter_creature_params)
+    
+        render json: @encounter_creature
+        end
+    
+        def update
+        @encounter_creature = Encounter_Creatures.find(params[:id])
+        @encounter_creature.update!(encounter_creature_params)
+    
+        render json: @encounter_creature
+        end
+    
+        def destroy
+        @encounter_creature = Encounter_Creatures.find(params[:id]).destroy
+        render status: :ok
+        end
+    
+        private
+        def encounter_creature_params
+            params.require(:encounter_creature).permit(:creatures)
+        end
+
+
+end
