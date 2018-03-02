4091f5f0ff205f6629ed430c4c1b3c481bf90368
JasenABaker <JasenABaker@gmail.com>
2018-03-02 13:27:49 -0500

    finsihed styling

--- client/src/components/AdventureTab.jsx
+++ client/src/components/AdventureTab.jsx
@@ -1,61 +1,84 @@
-import React from 'react'
+import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
-import {
-    Accordion,
-    AccordionItem,
-    AccordionItemTitle,
-    AccordionItemBody,
-} from 'react-accessible-accordion';
 
 
 
+let hooks = null
+let run = null
+let add = null
+
+class AdventureTab extends Component {
 
 
-const AdventureTab = (props) => {
-    return (
-        <Accordion>
-            <AccordionItem>
-            <AccordionItemTitle>
-            <h1>{props.adventure.title}</h1>
-            </AccordionItemTitle>
-                    <AccordionItemBody>
-                    <p>{props.adventure.intro}</p>
-                </AccordionItemBody>
-            </AccordionItem>
-                <details>
-                    <summary><h2>Adventure Synopsis</h2></summary>
-                    <p>{props.adventure.synopsis}</p>
+    render() {
+        if (this.props.adventure.hooks) {
+            hooks = <details>
+                <summary>Hooks</summary>
+                <p>{this.props.adventure.hooks_intro}</p>
+
+                {this.props.adventure.hooks.map((hook) => {
+                    return (
+                        <details>
+                            <summary>{hook.title}</summary>
+                            <p>{hook.description}</p>
+                        </details>
+                    )
+
+                })}
+            </details>
+        } else {
+            hooks = null
+        }
+
+        if(this.props.adventure.running_the_adventure){
+            run = <details>
+                    <summary>Running the Adventure</summary>
+                    <p>{this.props.adventure.running_the_adventure}</p>
+
                 </details>
-                <details>
-                    <summary><h2>Character Hooks</h2></summary>
-                    <p>{props.adventure.hooks_intro}</p>
-
-                    {props.adventure.hooks.map((hook) => {
-                        return (
-                            <div>
-                                <details>
-                                    <summary><h3>{hook.title}</h3></summary>
-                                    <p>{hook.description}</p>
-                                </details>
-                            </div>
+        } else {
+            run = null
+        }
+        if(this.props.adventure.additional_info){
+            add = <details>
+                    <summary>Additonal Information</summary>
+                    {this.props.adventure.additional_info.map((info)=>{
+                        return(
+                            <details>
+                                <summary>{info.title}</summary>
+                                <p>{info.description}</p>
+                            </details>
                         )
                     })}
 
                 </details>
-                {props.adventure.additional_info.map((info) => {
-                    return (
-                        <div>
-                            <details>
-                                <summary><h4>{info.title}</h4></summary>
-                                <p>{info.description}</p>
-                            </details>
-                        </div>
-                    )
-                })}
-                
-        </Accordion>
-    )
+        }
 
+
+        return (
+            <details>
+                <summary>
+                    {this.props.adventure.title}
+        </summary>
+                <details>
+                    <summary>Introduction</summary>
+                    <p>{this.props.adventure.intro}</p>
+                </details>
+                <details>
+                    <summary>
+                        Adventure Synopsis
+                    </summary>
+
+                    <p>{this.props.adventure.synopsis}</p>
+                </details>
+                {run}
+                {hooks}
+                {add}
+            </details>
+
+        )
+
+    }
 }
 
 export default AdventureTab
---
+++ client/src/components/styled components/ClickPages.jsx
@@ -0,0 +1,31 @@
+import styled from 'styled-components'
+
+
+
+
+
+export const MainPage = styled.details`
+    height: 100%;
+    width: 100%;
+    overflow: scroll;
+    
+`
+
+export const SubPage = styled.details`
+    height: 20vh;
+    width: 100%;
+    overflow: scroll;
+
+`
+
+export const FirstHeader = styled.summary`
+    wdith: 100%;
+    height: 10vh;
+
+
+`
+
+export const SecondHeader = styled.summary`
+        height: 5vh;
+        width:
+`
