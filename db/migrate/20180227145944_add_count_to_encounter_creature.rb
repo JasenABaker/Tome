e536b109b446380b3c1b65c9b0b8f33dd9cc3b53
JasenABaker <JasenABaker@gmail.com>
2018-03-05 10:57:58 -0500

    more migration

--- db/migrate/20180227145944_add_count_to_encounter_creature.rb
+++ db/migrate/20180227145944_add_count_to_encounter_creature.rb
@@ -1,4 +1,12 @@
 class AddCountToEncounterCreature < ActiveRecord::Migration[5.1]
+  create_table "encounter_creatures", force: :cascade do |t|
+    t.jsonb "creatures"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "encounter_id"
+    t.integer "count"
+    t.index ["encounter_id"], name: "index_encounter_creatures_on_encounter_id"
+  end
   def change
     add_column :encounter_creatures, :count, :integer
   end
