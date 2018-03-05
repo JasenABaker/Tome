fe640e290ba9b2d6f390767f3f0fe7dfa986dd16
JasenABaker <JasenABaker@gmail.com>
2018-03-05 09:24:17 -0500

    changed migration

--- db/migrate/20180225044546_add_encounter_to_encounter_creature.rb
+++ db/migrate/20180225044546_add_encounter_to_encounter_creature.rb
@@ -1,5 +1,5 @@
 class AddEncounterToEncounterCreature < ActiveRecord::Migration[5.1]
   def change
-    add_reference :encounter_creatures, :encounter, foreign_key: true
+    add_reference :encounter_creature, :encounter, foreign_key: true
   end
 end
--- db/migrate/20180227145944_add_count_to_encounter_creature.rb
+++ db/migrate/20180227145944_add_count_to_encounter_creature.rb
@@ -1,5 +1,5 @@
 class AddCountToEncounterCreature < ActiveRecord::Migration[5.1]
   def change
-    add_column :encounter_creature, :count, :integer
+    add_column :encounter_creatures, :count, :integer
   end
 end
